46536225f320674a4f554f8ee9a69a56135d29f4
Julian Engel <julian@julosdevelopment.com>
2015-08-01 18:40:00 -0400

    added a new class , restarted and added Core Location

---
+++ ParseSwiftStarterProject/ParseStarterProject/TableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewController.swift
+//  ParseStarterProject
+//
+//  Created by Julian Engel on 8/1/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseUI
+
+class TableViewController : PFQueryTableViewController, CLLocationManagerDelegate 
+{
+    
+}
