63a87801f1eeb5988383b047f62efc17c3fe3f68
Julian Engel <julian@julosdevelopment.com>
2015-08-01 20:48:27 -0400

    nothing much

--- ParseSwiftStarterProject/ParseStarterProject/TimelineViewController.swift
+++ ParseSwiftStarterProject/ParseStarterProject/TimelineViewController.swift
@@ -1,18 +1,16 @@
 //
 //  TimelineViewController.swift
-//  ParseStarterProject
+//  Makestagram
 //
-//  Created by Julian Engel on 8/1/15.
-//  Copyright (c) 2015 Parse. All rights reserved.
+//  Created by Benjamin Encz on 5/21/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
 //
 
 import UIKit
 import Parse
 import ConvenienceKit
 
-class TimelineViewController: UIViewController {
-    
-    var uploader : UploadViewController?
+class TimelineViewController: UIViewController{
     
     
-  }
+   }
