ea02df0a25f5b601b96ae56e805b750128ef2f66
airrick213 <erickim213@gmail.com>
2015-08-02 09:44:52 -0400

    added button functionality for cells

--- ParseSwiftStarterProject/ParseStarterProject/TimelineTableViewCell.swift
+++ ParseSwiftStarterProject/ParseStarterProject/TimelineTableViewCell.swift
@@ -21,15 +21,39 @@ class TimelineTableViewCell: UITableViewCell {
     var post: Post?
     
     @IBAction func upvoteButtonTapped(sender: AnyObject) {
-        
+        if upvoteButton.selected {
+            upvoteButton.selected = false
+            voteCountLabel.text = "\((NSString(string: voteCountLabel.text!).intValue - 1))"
+        }
+        else if downvoteButton.selected{
+            upvoteButton.selected = true
+            downvoteButton.selected = false
+            voteCountLabel.text = "\((NSString(string: voteCountLabel.text!).intValue + 2))"
+        }
+        else {
+            upvoteButton.selected = true
+            voteCountLabel.text = "\((NSString(string: voteCountLabel.text!).intValue + 1))"
+        }
     }
     
     @IBAction func downvoteButtonTapped(sender: AnyObject) {
-        
+        if downvoteButton.selected {
+            downvoteButton.selected = false
+            voteCountLabel.text = "\((NSString(string: voteCountLabel.text!).intValue + 1))"
+        }
+        else if upvoteButton.selected{
+            downvoteButton.selected = true
+            upvoteButton.selected = false
+            voteCountLabel.text = "\((NSString(string: voteCountLabel.text!).intValue - 2))"
+        }
+        else {
+            downvoteButton.selected = true
+            voteCountLabel.text = "\((NSString(string: voteCountLabel.text!).intValue - 1))"
+        }
     }
     
     @IBAction func flagButtonTapped(sender: AnyObject) {
-        
+        flagButton.selected = !flagButton.selected
     }
     
     @IBAction func commentsButtonTapped(sender: AnyObject) {
